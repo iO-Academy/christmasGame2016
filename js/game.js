68a27f845d48c459b8d1dd68557fe764f8a15bfc
Aimee Wyld <aimee.eloise@mail.com>
2016-10-24 15:59:39 +0100

    tidying up

--- js/game.js
+++ js/game.js
@@ -2,9 +2,22 @@ $(function() {
     $(document).keydown(function(e) {
         var levelNo = $(".selection").length
         var rand = Math.ceil(Math.random() * levelNo)
+        var selLev
+        var levWidth
+        var canvasWidth
         if (e.keyCode == 32) {
-            var selLev = "#selection" + rand
-            var levWidth = $(selLev).width()
+            selLev = "#selection" + rand
+            levWidth = $(selLev).width()
+            console.log(levWidth)
+            canvasWidth = $("#game").width()
+            console.log(canvasWidth)
+            $(selLev).appendTo("#game")
+            $(selLev).css({
+                "left": canvasWidth
+            })
+            $(selLev).animate({
+                left: -(levWidth - canvasWidth)
+                }, 3000)
         }
     })
 
