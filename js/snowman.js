98a68fa0657ac47a0e74e914871cf1197a44dd5a
Rhymes <rhymes@triplejdeveloping.com>
2016-10-24 16:55:39 +0100

    moving snowman

--- js/snowman.js
+++ js/snowman.js
@@ -1,20 +1,22 @@
 $(function() {
 
-    function moveSnowman() {
-
-        var startingPositionY = $("#snowman").css("top")
+        var gameHeight = $("#game").height() - 5
+        var snowmanHeight = $("#snowman").height()
 
         $(document).keydown(function (e) {
-            if (e.keyCode == 40) {
+            console.log("sausage")
+            var startingPositionY = $("#snowman").css("top")
+            var positionNumber = startingPositionY.substring(0, (startingPositionY.length - 2))
+            if (e.keyCode == 40 && positionNumber <= (gameHeight - snowmanHeight)) {
                 startingPositionY = $("#snowman").css("top", "+=5px")
-                $('#snowman').animate({top: startingPositionY})
-                console.log(startingPositionY)
+                $('#snowman').animate({top: startingPositionY}, "fast")
             }
 
-            if (e.keyCode == 38) {
+            if (e.keyCode == 38 && positionNumber > 0) {
                 startingPositionY = $("#snowman").css("top", "-=5px")
-                $('#snowman').animate({top: startingPositionY})
+                $('#snowman').animate({top: startingPositionY}, "fast")
             }
         })
-    }
+
 })
+
