68691718a8ebfe10d3a357a104bec07f757cac6f
Rhymes <rhymes@triplejdeveloping.com>
2016-10-24 09:56:47 +0100

    updated splash js

--- js/splashPage.js
+++ js/splashPage.js
@@ -1,19 +1,25 @@
 $(function() {
+
     $('form').submit(function() {
         var obj = {}
         obj.name = $('#username').val()
         obj.email = $('#email').val()
-        $.ajax({
-            method: "POST",
-            url: "POST.php",
-            data: obj
-        })
-            .done(
-                function(a) {
-                    console.log(a)
 
-                })
-        return false;
+        if (validateForm()) {
+            $.ajax({
+                method: "POST",
+                url: "POST.php",
+                data: obj
+            })
+                .done(
+                    function (a) {
+                        console.log(a)
+
+                    })
+            return false;
+        } else {
+            $('#email').insertAfter("Invalid username or email. Please try again.")
+        }
     })
 
 
