24ba3e871034d6507ab2763c45b1986ee4a811b0
Aimee Wyld <aimee.eloise@mail.com>
2016-10-24 16:13:07 +0100

    Made animation linear and added level gap.

--- js/game.js
+++ js/game.js
@@ -8,16 +8,14 @@ $(function() {
         if (e.keyCode == 32) {
             selLev = "#selection" + rand
             levWidth = $(selLev).width()
-            console.log(levWidth)
             canvasWidth = $("#game").width()
-            console.log(canvasWidth)
             $(selLev).appendTo("#game")
             $(selLev).css({
                 "left": canvasWidth
             })
             $(selLev).animate({
-                left: -(levWidth - canvasWidth)
-                }, 3000)
+                left: -(levWidth - canvasWidth + 50)
+                }, 3000, "linear")
         }
     })
 
