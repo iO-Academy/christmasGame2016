bace5fc61ac36ba33f9f23e5e4c2877bd4bbe164
Rhymes <rhymes@triplejdeveloping.com>
2016-10-24 15:06:29 +0100

    added snowman

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 $(function() {
-    $(document).keydown(function(e) {
+    var startingPositionY = $("#snowman").css("top")
+
+    $(document).keydown(function (e) {
         var levelNo
         var rand = Math.ceil(Math.random() * levelNo)
         console.log(rand)
@@ -8,5 +10,24 @@ $(function() {
         }
     })
 
+    function moveSnowman() {
+        $("#game").keydown(function (e) {
+            if (e.keyCode == 40 && (startingPositionY <= 485 - 50)) {
+                // startingPositionY += 5
+                // $("#snowman").css("top", startingPositionY)
+                $('#snowman').animate({top: "+=5px"}, 'fast')
+            } else {
+                startingPositionY = startingPositionY += 0
+            }
 
+            if (e.keyCode == 38 && (startingPositionY >= 0)) {
+                // startingPositionY -= 5
+                // $("#snowman").css("top", startingPositionY)
+                $('#snowman').animate({top: "-=5px"}, 'fast')
+            }
+        })
+        console.log(startingPositionY)
+    }
+
+    moveSnowman()
 })
