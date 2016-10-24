b18809a28f8ca1c2df79243d395add6154717798
Rhymes <rhymes@triplejdeveloping.com>
2016-10-24 15:42:53 +0100

    basic snowman movement

--- js/game.js
+++ js/game.js
@@ -1,6 +1,4 @@
 $(function() {
-    var startingPositionY = $("#snowman").css("top")
-
     $(document).keydown(function (e) {
         var levelNo
         var rand = Math.ceil(Math.random() * levelNo)
@@ -10,24 +8,5 @@ $(function() {
         }
     })
 
-    function moveSnowman() {
-        $("#game").keydown(function (e) {
-            if (e.keyCode == 40 && (startingPositionY <= 485 - 50)) {
-                // startingPositionY += 5
-                // $("#snowman").css("top", startingPositionY)
-                $('#snowman').animate({top: "+=5px"}, 'fast')
-            } else {
-                startingPositionY = startingPositionY += 0
-            }
 
-            if (e.keyCode == 38 && (startingPositionY >= 0)) {
-                // startingPositionY -= 5
-                // $("#snowman").css("top", startingPositionY)
-                $('#snowman').animate({top: "-=5px"}, 'fast')
-            }
-        })
-        console.log(startingPositionY)
-    }
-
-    moveSnowman()
 })
