3bfdd55935c1aa081be27879737124969a0a0625
Rhymes <rhymes@triplejdeveloping.com>
2016-10-24 09:18:16 +0100

    first script update

--- js/script.js
+++ js/script.js
@@ -1,47 +1,20 @@
-$(document).ready(function()
-{
+$(function() {
+    $('form').submit(function() {
+        var obj = {}
+        obj.name = $('#username').val()
+        obj.email = $('#email').val()
+        $.ajax({
+            method: "POST",
+            url: "POST.php",
+            data: obj
+        })
+            .done(
+                function(a) {
+                    console.log(a)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                })
+        return false;
+    })
 
 
 
