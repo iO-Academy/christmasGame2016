c6664dbd418896164848f71eb6d724dc38f4a9fb
Aimee Wyld <aimee.eloise@mail.com>
2016-10-26 16:23:23 +0100

    Split into two animations to remove gap between levels.

--- js/game.js
+++ js/game.js
@@ -1,67 +1,79 @@
 $(function() {
 
-    function gameLoop(counter, totalLevels, canvasWidth, initialSpeed, speed, increaseFactor, maxSpeed) {
-            var rand = Math.ceil(Math.random() * totalLevels),
-            selectedLevel = ".level" + rand,
-            levWidth = $(selectedLevel).width(),
-            $selectedLevel = $(selectedLevel),
-            loadedLevel = "#game " + selectedLevel,
-            speedIncrementer = Math.floor(counter/totalLevels),
-            dur
-
-            if (speed >= maxSpeed) {
-                speed = maxSpeed
-            } else {
-                speed = initialSpeed * Math.pow(increaseFactor, speedIncrementer)
-            }
-            dur = levWidth/speed
-            counter++
-
-            $selectedLevel.clone().appendTo("#game")
-            var $loadedLevel = $(loadedLevel)
-            $loadedLevel.css({
-                "left": canvasWidth
-            })
-            $loadedLevel.animate({
-                left: -levWidth 
-            },
-                dur,
-                "linear",
-                function() {
-                    $loadedLevel.remove()
-                    requestAnimationFrame(function() {
-                        gameLoop(counter, totalLevels, canvasWidth, initialSpeed, speed, increaseFactor, maxSpeed)
-                    })
-                }
-            )
-    }
-
     function gameStart(e) {
-        var counter = 0,
-            totalLevels = $("#levelContainer .level").length,
-            canvasWidth = $("#game").width(),
-            initialSpeed = 0.07466,
+        var initialSpeed = 0.08,
             speed = initialSpeed,
-            increaseFactor = 1.2,
-            maxSpeed = initialSpeed * Math.pow(increaseFactor, 7) //0.2675202785
+            increaseFactor = 1.1,
+            counter = 0,
+            maxSpeed = initialSpeed * Math.pow(increaseFactor, 7)
         if (e.keyCode == 32 && inPlay == false) {
             inPlay = true
             moveSnowman()
-            gameLoop(counter, totalLevels, canvasWidth, initialSpeed, speed, increaseFactor, maxSpeed)
+            gameLoop(speed, maxSpeed, increaseFactor, initialSpeed, counter)
         }
+    }
+
+    function gameLoop(speed, maxSpeed, increaseFactor, initialSpeed, counter) {
+        var $loadedLevel,
+            totalLevels = $("#levelContainer .level").length,
+            canvasWidth = $("#game").width(),
+            speedIncrementer = Math.floor(counter / totalLevels)
+        if (speed >= maxSpeed) {
+            speed = maxSpeed
+        } else if (initialSpeed * Math.pow(increaseFactor, speedIncrementer) > maxSpeed) {
+           speed = maxSpeed
+        } else {
+            speed = initialSpeed * Math.pow(increaseFactor, speedIncrementer)
+        }
+        counter++
+        $loadedLevel = load(canvasWidth, totalLevels, counter, increaseFactor, speedIncrementer)
+
+        animate1($loadedLevel, canvasWidth, speed, maxSpeed, increaseFactor, initialSpeed, counter, totalLevels)
+    }
+
+    function load(canvasWidth, totalLevels, counter, increaseFactor, speedIncrementer) {
+        var rand = Math.ceil(Math.random() * totalLevels),
+            selectedLevel = "#levelContainer .level" + rand,
+            $selectedLevel = $(selectedLevel),
+            $loadedLevel
+        $selectedLevel.clone().appendTo("#game")
+        $loadedLevel = $("#game .level")
+        $loadedLevel.last().css({
+            "left": canvasWidth + 150
+        })
+        return $loadedLevel
+    }
+
+    function animate1($loadedLevel, canvasWidth, speed, maxSpeed, increaseFactor, initialSpeed, counter, totalLevels) {
+        var level = $loadedLevel.last(),
+            levWidth = level.width(),
+            dur1 = levWidth / speed
+        if (counter % totalLevels == 0) {
+            var wait = (dur1/8)
+            level.delay(wait)
+        }
+                level.animate({
+            left: - (levWidth - canvasWidth)
+        }, dur1, "linear", function() {
+                    animate2(level, canvasWidth, speed)
+           gameLoop(speed, maxSpeed, increaseFactor, initialSpeed, counter)
+        })
+    }
+
+    function animate2(level, canvasWidth, speed) {
+        var
+            levWidth = level.width(),
+        dur2 = canvasWidth / speed
+        level.animate({
+            left: - levWidth
+        }, dur2, "linear", function() {
+            level.remove()
+        })
     }
 
     var inPlay = false
     $(document).keydown(function(e) {
         gameStart(e)
     })
+
 })
-
-
-// console.log("rand: " + rand + "||",
-//     "selectedLevel: " + selectedLevel + "||",
-//     "$selectedLevel: " + $selectedLevel + "||",
-//     "loadedLevel: " + loadedLevel + "||",
-//     "$loadedLevel: " + $loadedLevel + "||"
-// )
-// console.log($selectedLevel)
