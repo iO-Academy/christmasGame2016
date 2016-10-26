b49fb2f23b0ae26e6a3774f6741694ab7a5188bd
Rhymes <rhymes@triplejdeveloping.com>
2016-10-26 14:53:15 +0100

    added ajax request

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,7 +1,34 @@
 $(function() {
     $("#leaderboard").click(function() {
+        var ajaxErrorMessage = "Unexpected error. Please try again."
+        var obj = {}
+        obj.action = "getLeaderboard"
+
         openPopup($("#leaderScreen"))
+
+        $.ajax({
+            method: "POST",
+            url: "api/",
+            data: obj,
+            success: function(response) {
+                console.log(response)
+                if (!response.success) {
+                    $("#error").text(ajaxErrorMessage)
+                } else {
+
+                    $.each(response.data.leaderboard, function (key, person) {
+                        $("#leaderScreen").append(person.name + ": " + person.time + "<br>")
+                    })
+                }
+            },
+            error: function(data) {
+                console.log(data)
+                $("#error").text(ajaxErrorMessage)
+            }
+        })
     })
 
 
 })
+
+
