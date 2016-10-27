c3f6716a00a50dbc5ba4c0965ff4988fd640a073
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-10-27 14:42:17 +0100

    Moved some comments from DocBlocks into function defs

--- js/game.js
+++ js/game.js
@@ -1,13 +1,5 @@
 
 /**
- * Sets:
- * initialSpeed, the speed of level movement when the game begins (px/ms);
- * speed, the current speed of level movement (px/ms);
- * increaseFactor, the amount speed is multiplied by on every increase;
- * counter, indicates the number of times gameLoop() has been executed, i.e. how many levels have passed through the
- *  game area;
- * maxSpeed, the upper limit for level movement speed (px/ms).
- *
  * Activated by a keydown.
  * If the key pressed is the space bar, then:
  * the keydown listener with gameStart() as the callback method is turned off;
@@ -17,11 +9,12 @@
  * @param e OBJECT keydown event
  */
 function gameStart(e) {
-    var initialSpeed = 0.09,
-        speed = initialSpeed,
-        increaseFactor = 1.1,
-        counter = 0,
-        maxSpeed = initialSpeed * Math.pow(increaseFactor, 7)
+    var initialSpeed = 0.09, //the speed of level movement when the game begins (px/ms)
+        speed = initialSpeed, //the current speed of level movement (px/ms)
+        increaseFactor = 1.1, //the amount speed is multiplied by on every increase
+        counter = 0, //indicates the number of times gameLoop() has been executed, i.e. how many levels have passed
+                     //through the game area
+        maxSpeed = initialSpeed * Math.pow(increaseFactor, 7) //the upper limit for level movement speed (px/ms)
     if (e.keyCode == 32) {
         $(document).off('keydown', gameStart)
         moveSnowman()
@@ -30,14 +23,6 @@ function gameStart(e) {
 }
 
 /**
- * Sets:
- * $loadedLevel, a jQuery OBJECT whose properties 0 & 1 contain the two levels (DOM OBJECTS) that are currently loaded
- *      into #game;
- * totalLevels, the total number of levels available for play;
- * gameWidth, the width of the game area inside the white frame (px);
- * speedIncrementer, records the counter, modulo the total number of levels, e.g. if there are 8 levels in total, this
- *      increments by 1 every time 8 levels pass through the game area.
- *
  * Multiplies speed by increaseFactor if counter is a multiple of totalLevels, capping it at maxSpeed if exceeded.
  * Increments counter by 1
  * Calls load() and assigns the returned value to $loadedLevel.
@@ -51,10 +36,13 @@ function gameStart(e) {
  * through the game area
  */
 function gameLoop(speed, maxSpeed, increaseFactor, initialSpeed, counter) {
-    var $loadedLevel,
-        totalLevels = $("#levelContainer .level").length,
-        gameWidth = $("#game").width(),
-        speedIncrementer = Math.floor(counter / totalLevels)
+    var $loadedLevel, //a jQuery OBJECT whose properties 0 & 1 contain the two levels (DOM OBJECTS) that are currently
+                      //loaded into #game
+        totalLevels = $("#levelContainer .level").length, //the total number of levels available for play
+        gameWidth = $("#game").width(), //the width of the game area inside the white frame (px)
+        speedIncrementer = Math.floor(counter / totalLevels) //records the counter, modulo the total number of levels,
+                                                             //e.g. if there are 8 levels in total, this increments by
+                                                             //1 every time 8 levels pass through the game area
     if (speed >= maxSpeed) {
         speed = maxSpeed
     } else if (initialSpeed * Math.pow(increaseFactor, speedIncrementer) > maxSpeed) {
@@ -69,14 +57,6 @@ function gameLoop(speed, maxSpeed, increaseFactor, initialSpeed, counter) {
 }
 
 /**
- * Sets:
- * rand, a random integer between 1 and totalLevels (inclusive), indicating the next level to be loaded;
- * selectedLevel, a STRING containing the CSS selector for the level determined by rand;
- * $selectedLevel, a jQuery OBJECT containing the next level (the DOM OBJECT selected by selectedLevel) to be loaded
- *      into #game;
- * $loadedLevel, a jQuery OBJECT whose properties 0 & 1 contain the two levels (DOM OBJECTS) that are currently loaded
- *      into #game;
- *
  * Randomly selects a level, clones it and appends the clone to #game, moves it to the right of the game area, ready for
  * animation and updates $loadedLevel.
  *
@@ -92,10 +72,13 @@ function gameLoop(speed, maxSpeed, increaseFactor, initialSpeed, counter) {
  * currently loaded into #game
  */
 function load(gameWidth, totalLevels, counter, increaseFactor, speedIncrementer) {
-    var rand = Math.ceil(Math.random() * totalLevels),
-        selectedLevel = "#levelContainer .level" + rand,
-        $selectedLevel = $(selectedLevel),
-        $loadedLevel
+    var rand = Math.ceil(Math.random() * totalLevels), //a random integer between 1 and totalLevels (inclusive),
+                                                       //indicating the next level to be loaded
+        $selectedLevel = $(selectedLevel), //a jQuery OBJECT containing the next level (DOM OBJECT) to be loaded into
+                                           //#game, determined by a CSS selector containing rand
+        $loadedLevel //a jQuery OBJECT whose properties 0 & 1 contain the two levels (DOM OBJECTS) that are currently
+                     // loaded into #game
+
     $selectedLevel.clone().appendTo("#game")
     $loadedLevel = $("#game .level")
     $loadedLevel.last().css({
@@ -105,13 +88,6 @@ function load(gameWidth, totalLevels, counter, increaseFactor, speedIncrementer)
 }
 
 /**
- * Sets:
- * level, a jQuery OBJECT whose property 0 contains the most recently loaded level (DOM OBJECT), that which is to be
- *      animated;
- * levWidth, the width of level (px);
- * dur1, the duration of animation (ms);
- * wait, the delay put onto the animation of the first level after a speed increase.
- *
  * Calculates the duration of animation that will ensure that its movement speed is the same as the current value of the
  * speed variable.
  * Applies a delay to the first level after a speed increase to prevent the obstacles from catching up with those in the
@@ -132,11 +108,12 @@ function load(gameWidth, totalLevels, counter, increaseFactor, speedIncrementer)
  * @param totalLevels NUMBER totalLevels NUMBER the total number of levels available for play
  */
 function animate1($loadedLevel, gameWidth, speed, maxSpeed, increaseFactor, initialSpeed, counter, totalLevels) {
-    var level = $loadedLevel.last(),
-        levWidth = level.width(),
-        dur1 = levWidth / speed
+    var level = $loadedLevel.last(), //a jQuery OBJECT whose property 0 contains the most recently loaded level
+                                     //(DOM OBJECT), that which is to be animated
+        levWidth = level.width(), //the width of level (px)
+        dur1 = levWidth / speed //the duration of animation (ms)
     if (counter % totalLevels == 0) {
-        var wait = (dur1/8)
+        var wait = (dur1/8) //the delay put onto the animation of the first level after a speed increase
         level.delay(wait)
     }
             level.animate({
@@ -148,11 +125,6 @@ function animate1($loadedLevel, gameWidth, speed, maxSpeed, increaseFactor, init
 }
 
 /**
- * Sets:
- * levWidth, the width of level (px);
- * dur2, the duration of animation that will ensure that its movement speed is the same as the value of the speed
- *      variable (ms).
- *
  * Calculates the duration of animation that will ensure that its movement speed is the same as the current value of the
  * speed variable.
  * Animates level to transition leftwards from its current position (right-aligned with the game area) until its right edge
@@ -166,8 +138,8 @@ function animate1($loadedLevel, gameWidth, speed, maxSpeed, increaseFactor, init
  */
 function animate2(level, gameWidth, speed) {
     var
-        levWidth = level.width(),
-    dur2 = gameWidth / speed
+        levWidth = level.width(), //the width of level (px)
+    dur2 = gameWidth / speed //the duration of animation (ms)
     level.animate({
         left: - levWidth
     }, dur2, "linear", function() {
