6b46a75b7d20f3bdea5d9fe53a86428105ea8786
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2016-10-28 11:29:52 +0100

    fix broken timer merge variables

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,6 @@
 // set global variable for use in functions
 var timer
 var gamePlaySeconds
-var padLength
 var paddedMinutes
 var paddedSeconds
 
@@ -38,8 +37,6 @@ function stopTimer() {
 function startTimer() {
     gamePlaySeconds = 0
     var padLength = "00"
-    var i = 0
-    padLength = "00"
     paddedMinutes = "00"
     paddedSeconds = "00"
     $("#timer").html(paddedMinutes + ":" + paddedSeconds)
@@ -47,7 +44,7 @@ function startTimer() {
     timer = setInterval(function () {
 
         // increase the counter by one on each 1000ms interval
-        i++
+        gamePlaySeconds++
 
         // turn game minutes to a string for correct display
         var gameMinutes = getGameMinutes(gamePlaySeconds) + ''
@@ -65,7 +62,7 @@ function startTimer() {
 }
 
 function resetTimer() {
-    i = 00
+    gamePlaySeconds = 00
     paddedMinutes = "00"
     paddedSeconds = "00"
 }
